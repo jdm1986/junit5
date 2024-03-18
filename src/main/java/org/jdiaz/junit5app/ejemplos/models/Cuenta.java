6ad9d1219773fc755004ffd44e377d84b299c1bd
sportyisus <diazmartinmail@gmail.com>
2024-03-18 11:43:55 +0100

    Terminando clase cuenta

--- src/main/java/org/jdiaz/junit5app/ejemplos/models/Cuenta.java
+++ src/main/java/org/jdiaz/junit5app/ejemplos/models/Cuenta.java
@@ -5,6 +5,15 @@ import java.math.BigDecimal;
 public class Cuenta {
     private String persona;
     private BigDecimal saldo;
+    private String nombre;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
     public String getPersona() {
         return persona;
