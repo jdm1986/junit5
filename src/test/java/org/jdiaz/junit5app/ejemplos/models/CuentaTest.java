0734746d677bce633ef9f5e0330eb1e2f6519afd
sportyisus <diazmartinmail@gmail.com>
2024-04-22 22:28:02 +0200

    TDD para debito y credito

--- src/test/java/org/jdiaz/junit5app/ejemplos/models/CuentaTest.java
+++ src/test/java/org/jdiaz/junit5app/ejemplos/models/CuentaTest.java
@@ -15,7 +15,7 @@ class CuentaTest {
 //        cuenta.setPersona("Andres");
         String esperado = "Andres";
         String realidad = cuenta.getPersona();
-
+        assertNotNull(realidad);
         Assertions.assertEquals(esperado,realidad);
         assertEquals("Andres", realidad);
     }
@@ -23,6 +23,7 @@ class CuentaTest {
     @Test
     void testSaldoCuenta() {
         Cuenta cuenta = new Cuenta("Andres", new BigDecimal("1000.12345"));
+        assertNotNull(cuenta.getSaldo());
         assertEquals(1000.12345, cuenta.getSaldo().doubleValue());
         assertFalse(cuenta.getSaldo().compareTo(BigDecimal.ZERO) < 0);
         assertTrue(cuenta.getSaldo().compareTo(BigDecimal.ZERO) > 0);
@@ -35,5 +36,23 @@ class CuentaTest {
 
 //        assertNotEquals(cuenta2, cuenta);
         assertEquals(cuenta2,cuenta);
+    }
+
+    @Test
+    void testDebitoCuenta() {
+        Cuenta cuenta = new Cuenta("Andres", new BigDecimal("1000.12345"));
+        cuenta.debito(new BigDecimal("100"));
+        assertNotNull(cuenta.getSaldo());
+        assertEquals(900, cuenta.getSaldo().intValue());
+        assertEquals("900.12345", cuenta.getSaldo().toPlainString());
+    }
+
+    @Test
+    void testCreditoCuenta() {
+        Cuenta cuenta = new Cuenta("Andres", new BigDecimal("1000.12345"));
+        cuenta.credito(new BigDecimal("100"));
+        assertNotNull(cuenta.getSaldo());
+        assertEquals(1100, cuenta.getSaldo().intValue());
+        assertEquals("1100.12345", cuenta.getSaldo().toPlainString());
     }
 }
